bf0e132cc6939bf17a44749ff8bc65b6034e0a25
Mallory-Mueller <mallory.m.mueller@gmail.com>
2021-11-29 20:34:11 -0600

    code practice True/False

---
+++ practice 11.28.py
@@ -0,0 +1,9 @@
+is_hot=False
+if is_hot:
+    print("It's a hot day")
+    print ("Drink lots of water")
+
+else:
+    print("It's a cold day")
+    print("Wear warm clothes")
+print ("Have a good day")
