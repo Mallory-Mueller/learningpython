c2c48fda7293ee7aa4caf2e1652cb31cad90a5d2
Mallory-Mueller <mallory.m.mueller@gmail.com>
2021-11-29 20:51:29 -0600

    I have added additional functions to this code

--- practice 11.28.py
+++ practice 11.28.py
@@ -1,9 +1,11 @@
 is_hot=False
+is_cold=False
 if is_hot:
     print("It's a hot day")
     print ("Drink lots of water")
-
-else:
+elif is_cold:
     print("It's a cold day")
     print("Wear warm clothes")
+
+else: print("It's a lovely day!")
 print ("Have a good day")
