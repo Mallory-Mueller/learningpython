ff72aa44f9988aa098ac42f29145906bacc3ff89
Mallory-Mueller <mallory.m.mueller@gmail.com>
2021-12-07 13:42:47 -0600

    First time with for loops

---
+++ For loop.py
@@ -0,0 +1,21 @@
+for item in "python":
+    print (item)
+
+for item in ["Mallory","Kevin",'Sarah']:
+        print (item)
+
+for item in [1,2,3,4,5,]:
+    print(item)
+
+for item in range (10):
+    print(item)
+
+for item in range (5,10,2):
+    print(item)
+for item in [10+20+30]:
+    print (item)
+prices =[20,30,40]
+total= 0
+for price in prices:
+    total +=price
+print(f"Total:{total}")
